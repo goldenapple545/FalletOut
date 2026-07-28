a0ac55617720ff7aea340e013964b236eaaa184f
Jordji <37375293+goldenapple545@users.noreply.github.com>
2026-07-28 10:32:56 +0400

    Install R3

---
+++ Assets/CodeBase/Infrastructure/R3Initializer.cs
@@ -0,0 +1,21 @@
+﻿// using UnityEngine;
+//
+// namespace CodeBase.CodeBase.Infrastructure
+// {
+//     public static class R3Initializer
+//     {
+//         [RuntimeInitializeOnLoadMethod(
+//             RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+//         private static void Initialize()
+//         {
+//             ObservableSystem.RegisterUnhandledExceptionHandler(
+//                 static exception => Debug.LogException(exception));
+//
+//             ObservableSystem.DefaultTimeProvider =
+//                 UnityTimeProvider.Update;
+//
+//             ObservableSystem.DefaultFrameProvider =
+//                 UnityFrameProvider.Update;
+//         }
+//     }
+// }
